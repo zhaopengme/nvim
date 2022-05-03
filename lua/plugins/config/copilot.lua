db26cb2923e970cfac0df99022f179a19d32d7c5
mikey.zhao <mikey.zhao@yeahmobi.com>
2022-05-04 00:15:19 +0800

    update

--- lua/plugins/config/copilot.lua
+++ lua/plugins/config/copilot.lua
@@ -1,9 +1,9 @@
 -- Copilot
-vim.b.copilot_enabled = false
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_assume_mapped = true
-
-vim.api.nvim_set_keymap("i", "<right>", 'copilot#Accept("")', {
-    expr = true,
-    silent = true
-})
+-- vim.b.copilot_enabled = false
+-- vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_assume_mapped = true
+--
+-- vim.api.nvim_set_keymap("i", "<right>", 'copilot#Accept("")', {
+--     expr = true,
+--     silent = true
+-- })
--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -1,72 +1,65 @@
 -- init.lua
-
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup {
-  disable_netrw        = true,
-  hijack_netrw         = true,
-  open_on_setup        = false,
-  ignore_ft_on_setup   = {},
-  auto_close           = false,
-  auto_reload_on_write = true,
-  open_on_tab          = false,
-  hijack_cursor        = false,
-  update_cwd           = false,
-  update_to_buf_dir    = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = false,
-    custom = {}
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = true,
+    ignore_ft_on_setup = {},
+    auto_reload_on_write = true,
+    open_on_tab = true,
+    hijack_cursor = true,
+    update_cwd = true,
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = ""
+        }
     },
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes"
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true
-  },
-  actions = {
-    change_dir = {
-      global = false,
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {}
     },
-    open_file = {
-      quit_on_open = false,
+    system_open = {
+        cmd = nil,
+        args = {}
+    },
+    filters = {
+        dotfiles = false,
+        custom = {}
+    },
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 500
+    },
+    view = {
+        width = 30,
+        height = 30,
+        hide_root_folder = false,
+        side = 'left',
+        mappings = {
+            custom_only = false,
+            list = {}
+        },
+        number = true,
+        relativenumber = true,
+        signcolumn = "yes"
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true
+    },
+    actions = {
+        change_dir = {
+            global = false
+        },
+        open_file = {
+            quit_on_open = false
+        }
     }
-  }
 }
