2e5110d79b39ca0da337ff6549d81c33bdc30a1a
mikey.zhao <mikey.zhao@yeahmobi.com>
2022-05-04 02:02:28 +0800

    update

--- lua/plugins/config/copilot.lua
+++ lua/plugins/config/copilot.lua
@@ -7,3 +7,8 @@
 --     expr = true,
 --     silent = true
 -- })
+--
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+
+vim.keymap.set("i", "<C-c>", 'copilot#Accept("<CR>")', { expr = true })
