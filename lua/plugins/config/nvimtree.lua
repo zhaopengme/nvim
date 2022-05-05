5477d6482e9566bb49729a3a7a41554e4363a88c
mikey.zhao <mikey.zhao@yeahmobi.com>
2022-05-05 23:41:34 +0800

    update

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -1,65 +1,65 @@
 -- init.lua
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-require'nvim-tree'.setup {
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_setup = true,
-    ignore_ft_on_setup = {},
-    auto_reload_on_write = true,
-    open_on_tab = true,
-    hijack_cursor = true,
-    update_cwd = true,
-    diagnostics = {
-        enable = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = ""
-        }
-    },
-    update_focused_file = {
-        enable = true,
-        update_cwd = true,
-        ignore_list = {}
-    },
-    system_open = {
-        cmd = nil,
-        args = {}
-    },
-    filters = {
-        dotfiles = false,
-        custom = {}
-    },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 500
-    },
-    view = {
-        width = 30,
-        height = 30,
-        hide_root_folder = false,
-        side = 'left',
-        mappings = {
-            custom_only = false,
-            list = {}
-        },
-        number = true,
-        relativenumber = true,
-        signcolumn = "yes"
-    },
-    trash = {
-        cmd = "trash",
-        require_confirm = true
-    },
-    actions = {
-        change_dir = {
-            global = false
-        },
-        open_file = {
-            quit_on_open = false
-        }
-    }
-}
+require("nvim-tree").setup({
+	disable_netrw = true,
+	hijack_netrw = true,
+	open_on_setup = true,
+	ignore_ft_on_setup = {},
+	auto_reload_on_write = true,
+	open_on_tab = true,
+	hijack_cursor = true,
+	update_cwd = true,
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	filters = {
+		dotfiles = false,
+		custom = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	view = {
+		width = 30,
+		height = 30,
+		hide_root_folder = false,
+		side = "left",
+		mappings = {
+			custom_only = false,
+			list = {},
+		},
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+	actions = {
+		change_dir = {
+			global = false,
+		},
+		open_file = {
+			quit_on_open = false,
+		},
+	},
+})
